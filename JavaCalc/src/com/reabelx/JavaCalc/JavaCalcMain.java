a84e49635bfd5aa3173974d749d90d4b6e4a8525
Ryan Reabel <ryanreabel@gmail.com>
2014-08-13 00:05:52 -0400

    JavaCalcMain Start
    nuff said.

---
+++ JavaCalc/src/com/reabelx/JavaCalc/JavaCalcMain.java
@@ -0,0 +1,10 @@
+package com.reabelx.JavaCalc;
+
+public class JavaCalcMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome. This is javaCalc v0.0.08132014");
+	}
+
+}
