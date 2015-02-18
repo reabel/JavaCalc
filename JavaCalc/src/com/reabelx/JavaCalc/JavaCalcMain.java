3ad2998fbfa7ac7b9cfdb5e25603d9bbb868110a
reabel <ryanreabel@gmail.com>
2015-02-17 22:26:52 -0500

    changed methods around so that input is gotten before case is hit, shortening required code. Also implemented % feature.

--- JavaCalc/src/com/reabelx/JavaCalc/JavaCalcMain.java
+++ JavaCalc/src/com/reabelx/JavaCalc/JavaCalcMain.java
@@ -15,119 +15,134 @@ public class JavaCalcMain {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(
 				System.in));
 		int menuSelect;
+		int firstNumber;
+		int secondNumber;
 		// Initialize the menu.
-		System.out.println("Welcome. This is javaCalc v0.0.10122014");
+		System.out.println("Welcome. This is javaCalc v0.0.02172015");
 		System.out
-				.println("Select a calculation: 1:Add, 2:Sub, 3:Mul, 4:Div :");
+				.println("Select a calculation: 1:Add, 2:Sub, 3:Mul, 4:Div, 5:Percent:");
 		// grab user input, if not an integer, throw error.
 		try {
 			menuSelect = Integer.parseInt(reader.readLine());
-
+			System.out.println("Enter #:");
+			firstNumber = Integer.parseInt(reader.readLine());
+			System.out.println("Enter #:");
+			secondNumber = Integer.parseInt(reader.readLine());
+			
 			switch (menuSelect) {
 			case 1:
-				add();
+				add(firstNumber, secondNumber);
 				break;
 			case 2:
-				subtract();
+				subtract(firstNumber, secondNumber);
 				break;
 			case 3:
-				multiply();
+				multiply(firstNumber, secondNumber);
 			case 4:
-				divide();
+				divide(firstNumber, secondNumber);
+			case 5:
+				percent(firstNumber, secondNumber);
 			default:
 				break;
 			}
 		} catch (NumberFormatException nfe) {
 			System.err.println("Please enter a number.");
-		} finally {
+		}catch(ArithmeticException zero){
+			System.err.println("You cannot divide by zero.");
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
 			System.out.println("Thank you for using this app.");
 		}
 
 	}
 	
-	public static void add(){
+	public static void add(int firstNumber, int secondNumber){
 		String added = "added";
-		int firstNumber; 
-		int secondNumber;
+//		int firstNumber; 
+//		int secondNumber;
 		try {
-			firstNumber = getNumber(added);
-			secondNumber = getNumber(added);
+			//firstNumber = getNumber(added);
+			//secondNumber = getNumber(added);
 			int result = firstNumber + secondNumber;
 			System.out.println(firstNumber+ "+" +secondNumber+ "="+result);
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		} 
 		
 		
 	}
 	
-	public static void subtract(){
+	public static void subtract(int firstNumber, int secondNumber){
 		String added = "subtracted";
-		int firstNumber; 
-		int secondNumber;
+//		int firstNumber; 
+//		int secondNumber;
 		try {
-			firstNumber = getNumber(added);
-			secondNumber = getNumber(added);
+//			firstNumber = getNumber(added);
+//			secondNumber = getNumber(added);
 			int result = firstNumber - secondNumber;
 			System.out.println(firstNumber+ "-" +secondNumber+ "="+result);
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 		
 	}
 	
-	public static void multiply(){
+	public static void multiply(int firstNumber, int secondNumber){
 		String actioned = "multiplied";
-		int firstNumber; 
-		int secondNumber;
+//		int firstNumber; 
+//		int secondNumber;
 		try {
-			firstNumber = getNumber(actioned);
-			secondNumber = getNumber(actioned);
+//			firstNumber = getNumber(actioned);
+//			secondNumber = getNumber(actioned);
 			int result = firstNumber * secondNumber;
 			System.out.println(firstNumber+ "*" +secondNumber+ "="+result);
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		} 
 		
 	}
 	
-	public static void divide(){
+	public static void divide(int firstNumber, int secondNumber){
 		String divided = "divided";
-		int firstNumber; 
-		int secondNumber;
+//		int firstNumber; 
+//		int secondNumber;
 		try {
-			firstNumber = getNumber(divided);
-			secondNumber = getNumber(divided);
+//			firstNumber = getNumber(divided);
+//			secondNumber = getNumber(divided);
 			int result = firstNumber / secondNumber;
 			System.out.println(firstNumber+ "/" +secondNumber+ "="+result);
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		} 
 		
 	}
+	
+	public static void percent(int firstNumber, int secondNumber){
+		String precent;
+//		int firstNumber;
+//		int secondNumber;
+		
+		try{
+			int result = (firstNumber / secondNumber) * 100;
+			System.out.println("Percent of "+firstNumber+" to "+secondNumber+" is:"+result+"%");
+		}catch (NumberFormatException e){
+			e.printStackTrace();
+		}
+	}
 
-	public static int getNumber(String calc) throws NumberFormatException, IOException{
+	/*public static int getNumber(String calc) throws NumberFormatException, IOException{
 		BufferedReader reader = new BufferedReader(new InputStreamReader(
 				System.in));
 		System.out.println("Please enter a number to be " +calc);
 		return Integer.parseInt(reader.readLine());
 		
-	}
+	}*/
 	
 }
